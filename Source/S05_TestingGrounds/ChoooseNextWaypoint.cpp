3b88a1d004b229e6c745bea1caed8183cd481147
vic_skl <vic_skl@mail.ru>
2018-12-24 20:45:36 +0300

    4

--- Source/S05_TestingGrounds/ChoooseNextWaypoint.cpp
+++ Source/S05_TestingGrounds/ChoooseNextWaypoint.cpp
@@ -1,11 +1,39 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChoooseNextWaypoint.h"
-
+#include "AIController.h"
+#include"PatrolRoute.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 
 EBTNodeResult::Type UChoooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	UE_LOG(LogTemp, Warning, TEXT("AI in C++  - yay"));
+	
+	
+
+	//Get the patrol route
+	auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+
+	if (!ensure(PatrolRoute)) { return EBTNodeResult::Failed;}
+	//Warn against empy patrol routes
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+	if(PatrolPoints.Num()==0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("A guard is missing patrol points"));
+		return EBTNodeResult::Failed;
+	}
+
+
+	//Set Next Waypoint
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+
+	//Cycle the index
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+
+
 	 return EBTNodeResult::Succeeded;
 }
--- Source/S05_TestingGrounds/ChoooseNextWaypoint.h
+++ Source/S05_TestingGrounds/ChoooseNextWaypoint.h
@@ -15,6 +15,10 @@ class S05_TESTINGGROUNDS_API UChoooseNextWaypoint : public UBTTaskNode
 	GENERATED_BODY()
 	
 		virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
-	
-	
-};
+protected:
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+		struct FBlackboardKeySelector IndexKey;
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+		struct FBlackboardKeySelector WaypointKey;
+	};
